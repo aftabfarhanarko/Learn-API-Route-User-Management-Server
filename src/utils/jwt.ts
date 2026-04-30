82ca61812e019dca224a66ee9d9305a7c72cfe56
Aftab Farhan <aftabfarhan324@gmail.com>
2026-04-30 18:40:21 +0600

    update port

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -2,10 +2,12 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRET, JWT_EXPIRES_IN } from "../config/env";
 
 export const generateToken = (userId: string): string => {
+  
   const secret: jwt.Secret = JWT_SECRET;
   return jwt.sign({ id: userId }, secret, {
     expiresIn: JWT_EXPIRES_IN,
   } as jwt.SignOptions); 
+};
 
 export const verifyToken = (token: string) => {
   return jwt.verify(token, JWT_SECRET as jwt.Secret);
