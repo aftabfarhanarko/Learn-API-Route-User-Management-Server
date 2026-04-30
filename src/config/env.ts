e1216b6ba81440c223261b3bf199e31471e49fc6
Aftab Farhan <aftabfarhan324@gmail.com>
2026-04-30 16:51:37 +0600

    dotenv.config done

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,15 @@
-// DATABASE_URL =
-//   "postgresql://postgres:arpo5432@localhost:5432/user-manage?schema=public";
-JWT_SECRET = "supersecretkey123";
-JWT_EXPIRES_IN = "7d";
+import dotenv from "dotenv";
+dotenv.config();
+
+function getEnvVar(key: string, required = true): string {
+  const value = process.env[key];
+  if (required && !value) {
+    throw new Error(`Environment variable ${key} is missing`);
+  }
+  return value!;
+}
+
+export const PORT = getEnvVar("PORT") || "6000";
+export const DATABASE_URL = getEnvVar("DATABASE_URL");
+export const JWT_SECRET = getEnvVar("JWT_SECRET");
+export const JWT_EXPIRES_IN = getEnvVar("JWT_EXPIRES_IN") || "7d";
